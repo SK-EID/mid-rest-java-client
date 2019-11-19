b8743837a78bd4bd47393a7365007a7ff50ac80e
Kristjan Jalukse <kristjan.jalukse@sk.ee>
2019-11-19 16:56:22 +0200

    Squashed commit of the pr/17:
    commit 3b7397b4dd7493529a3780bfe9e44212d97992c4
    commit 02941a1cbd6d3f04ffed2e2b9312ab0a52f94a8c
    commit 99c04fa096c93d631a4f664280c900ecc2e667ca
    commit 0fb1d4925d52d01c3dba05a44df4b433f9e03851
    commit 75e3e21d398541380072377d8249c1d370cbaece
    commit 321a8e6eb3aa7ff5b50fabda47b32cf592b5edca

---
+++ publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+project="mid-rest-java-client"
+
+version=$TRAVIS_TAG
+
+staging_url="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+repositoryId="ossrh"
+
+artifact=$project-$version
+
+gpg --import ./private.key
+
+mvn versions:set -DnewVersion=$TRAVIS_TAG
+
+mvn package
+
+gpg -ab pom.xml
+
+cd target
+
+gpg -ab $artifact.jar
+gpg -ab $artifact-sources.jar
+gpg -ab $artifact-javadoc.jar
+
+jar -cvf bundle.jar ../pom.xml ../pom.xml.asc $artifact.jar $artifact.jar.asc $artifact-javadoc.jar $artifact-javadoc.jar.asc $artifact-sources.jar $artifact-sources.jar.asc
+
+curl -ujorlina2 -u $SONATYPEUN:$SONATYPEPW --request POST -F "file=@bundle.jar" "https://oss.sonatype.org/service/local/staging/bundle_upload"
